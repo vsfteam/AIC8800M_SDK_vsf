f0306996f085c0aae8cc4231218246357517118f
vsfos <vsfos@foxmail.com>
2022-06-08 14:43:29 +0800

    add psram support

---
+++ plf/aic8800/src/driver/psram/psram_api.h
@@ -0,0 +1,14 @@
+#ifndef _PSRAM_API_H_
+#define _PSRAM_API_H_
+
+/**
+ * Init psram
+ */
+void psram_init(void);
+
+/**
+ * Get psram size
+ */
+unsigned int psram_size_get(void);
+
+#endif /* _PSRAM_API_H_ */
