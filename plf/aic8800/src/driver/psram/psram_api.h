a0f440dbceda2d1f7440c44c23c52b3972290b4c
vsfos <vsfos@foxmail.com>
2024-03-21 16:49:54 +0800

    [psram] update

--- plf/aic8800/src/driver/psram/psram_api.h
+++ plf/aic8800/src/driver/psram/psram_api.h
@@ -2,9 +2,20 @@
 #define _PSRAM_API_H_
 
 /**
- * Init psram
+ * @brief Init psram
+ * @param icache_size The size of memory accessed in icache, the remained in dcache
+ * Memory mapping (e.g. psram_size=0x00400000):
+ * +--------+ <----- cache_addr0(0x0C000000 - icache_size) -> psram_start(0x00000000)
+ * |        |   ^
+ * | icache |   | icache_size (>=0)
+ * |        |   v
+ * +--------+ <----- cache_addr1(0x0C000000) ->  psram_addr(icach_size)
+ * |        |   ^
+ * | dcache |   | remained_size
+ * |        |   v
+ * +--------+ <----- cache_addr2(0x0C000000 + remained_size) -> psram_end(0x00400000)
  */
-void psram_init(void);
+void psram_init(unsigned int icache_size);
 
 /**
  * Get psram size
